f669016fafb93118bd5b94c074104a16360f0f96
KirstA <andre.kirst@comparex.de>
2016-10-24 13:39:51 +0200

    Übernahme von anderem Projekt

--- 02_BankOCR/Program.cs
+++ 02_BankOCR/Program.cs
@@ -10,6 +10,21 @@ namespace _02_BankOCR
     {
         static void Main(string[] args)
         {
+            UI ui = new UI();
+            Interactors interactors = new Interactors();
+
+            ui.EnterGedrueckt += (anfang, ende) =>
+            {
+                IEnumerable<string> fizzBuzzListe = interactors.ErstelleFizzBuzzListe(anfang, ende);
+                ui.ZeigeListe(fizzBuzzListe);
+            };
+
+            string startText = interactors.GebeMirStarttext();
+            ui.ZeigeStarttext(startText);
+            ui.ZeigeDialog();
+
+            Console.WriteLine("Press the any key");
+            Console.ReadLine();
         }
     }
 }
---
+++ 02_BankOCR/UI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_BankOCR
+{
+    public class UI
+    {
+        public event Action<int, int> EnterGedrueckt;
+
+        public void OnEnter(int anfang, int ende)
+        {
+            EnterGedrueckt?.Invoke(anfang, ende);
+        }
+
+        public void ZeigeListe(IEnumerable<string> fizzBuzzListe)
+        {
+            foreach (string eintrag in fizzBuzzListe)
+            {
+                Console.WriteLine(eintrag);
+            }
+        }
+
+        public void ZeigeStarttext(string startText)
+        {
+            Console.Write(startText);
+        }
+
+        public void ZeigeDialog()
+        {
+            // "asdasd"
+            // "1,2"
+            string zeile = Console.ReadLine();
+            Tuple<int, int> zahlen = GibMirdieZahlenAusDerZeichenkette(zeile);
+            OnEnter(zahlen.Item1, zahlen.Item2);
+        }
+
+        private Tuple<int, int> GibMirdieZahlenAusDerZeichenkette(string zeile)
+        {
+            int[] split = zeile.Split(',').Select(x => Int32.Parse(x)).ToArray();
+            return new Tuple<int, int>(split[0], split[1]);
+        }
+    }
+}
