4050707e6877d5a9cbabc814e5128c2345f36f9d
KirstA <andre.kirst@comparex.de>
2016-10-24 15:39:24 +0200

    Hinzuf+gen zweier DAteien

---
+++ 02_BankOCR.Test/Test_ZeilenZuEintragConverter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace _02_BankOCR.Test
+{
+    [TestClass]
+    public class Test_ZeilenZuEintragConverter
+    {
+        [TestMethod]
+        public void Wenn_vier_Zeilen_dann_ein_Eintrag()
+        {
+            List<string> zeilen = new List<string>()
+            {
+                "    _  _     _  _  _  _  _ ",
+                "  | _| _||_||_ |_   ||_||_|",
+                "  ||_  _|  | _||_|  ||_| _|",
+                ""
+            };
+
+            List<Eintrag> eintraege = ZeilenZuEintragConverter.Convert(zeilen);
+            Assert.IsNotNull(eintraege);
+            Assert.AreEqual(eintraege.Count, 1);
+        }
+
+        [TestMethod]
+        public void Wenn_acht_Zeilen_dann_zwei_Eintraege()
+        {
+            List<string> zeilen = new List<string>()
+            {
+                "    _  _     _  _  _  _  _ ",
+                "  | _| _||_||_ |_   ||_||_|",
+                "  ||_  _|  | _||_|  ||_| _|",
+                "",
+
+                "    _  _     _  _  _  _  _ ",
+                "  | _| _||_||_ |_   || ||_|",
+                "  ||_  _|  | _||_|  ||_||_|",
+                ""
+            };
+
+            List<Eintrag> eintraege = ZeilenZuEintragConverter.Convert(zeilen);
+            Assert.IsNotNull(eintraege);
+            Assert.AreEqual(eintraege.Count, 2);
+
+            Eintrag eintrag1 = new Eintrag()
+            {
+                Zeilen = new List<string>()
+                {
+                    "    _  _     _  _  _  _  _ ",
+                    "  | _| _||_||_ |_   ||_||_|",
+                    "  ||_  _|  | _||_|  ||_| _|",
+                    ""
+                }
+            };
+
+            Eintrag eintrag2 = new Eintrag()
+            {
+                Zeilen = new List<string>()
+                {
+                    "    _  _     _  _  _  _  _ ",
+                    "  | _| _||_||_ |_   || ||_|",
+                    "  ||_  _|  | _||_|  ||_||_|",
+                    ""
+                }
+            };
+
+            List<Eintrag> eintrageErwartet = new List<Eintrag>()
+            {
+                eintrag1, eintrag2
+            };
+            CollectionAssert.AreEqual(eintrageErwartet[0].Zeilen, eintraege[0].Zeilen);
+        }
+    }
+}
---
+++ 02_BankOCR/EintragZuAccountnumberConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_BankOCR
+{
+    public class EintragZuAccountnumberConverter
+    {
+        public static List<int> PruefsummenBestimmen(Eintrag eintrag)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static List<List<string>> ZiffernExtrahieren(Eintrag eintrag)
+        {
+            return null;
+        }
+
+        internal static int PruefsummeFuerZifferBestimmen(List<string> ziffer)
+        {
+            return -1;
+        }
+
+        public static List<int> ZiffernDurchVergleichBestimmen(List<int> pruefsummen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Accountnumber ZuAccountnumberKonvertieren(List<int> ziffern)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
